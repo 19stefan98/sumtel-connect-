20b259448e675a4a47c1934b01761fe65c3dfe71
Степан Митрофанов <stepan.mitrofanov.2015@mail.ru>
2018-02-12 16:45:40 +0400

    conect

--- Check connection/Connect.cs
+++ Check connection/Connect.cs
@@ -20,10 +20,10 @@ namespace Check_connection
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl("http://itech:itech@saint-petersburg.sumtel.itech-test.ru/");
 
-            var street = (new WebDriverWait(driver, timeout)).Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id=\"check - connection - block\"]/form/div/div[3]/div/div[1]")));
+            var street = (new WebDriverWait(driver, timeout)).Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id=\"check - connection - block\"]/form/div/div[3]/div/div[1]/label/input")));
                 street.SendKeys("Пловдивская");
             var home = driver.FindElement(By.Name("home_corpus"));
-                home.SendKeys("5"); 
+                home.SendKeys("5");
                 home.Submit(); 
             Result = (new WebDriverWait(driver, timeout)).Until(ExpectedConditions.ElementIsVisible(By.ClassName("sa-icon sa-custom"))).Text;
         }
