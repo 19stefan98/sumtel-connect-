7009c58b01fb1aab6cd89295e2acc7093daae7e3
Степан Митрофанов <stepan.mitrofanov.2015@mail.ru>
2018-02-12 16:27:02 +0400

    a

---
+++ Check connection/Connect.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Check_connection
+{
+    public class Connection
+    {
+        public IWebDriver driver { get; set; }
+        public string Result { get; set; }
+        public Connection(IWebDriver driver)
+
+        {
+            this.driver = driver;
+        }
+
+        public void Action()
+        {
+            TimeSpan timeout = new TimeSpan(00, 00, 10);
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://itech:itech@voronezh.sumtel.itech-test.ru");
+
+            var street = (new WebDriverWait(driver, timeout)).Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id=\"check - connection - block\"]/form/div/div[3]/div/div[1]")));
+                street.SendKeys("Пловдивская");
+            var home = driver.FindElement(By.Name("home_corpus"));
+                home.SendKeys("5");
+                home.Submit(); 
+            Result = (new WebDriverWait(driver, timeout)).Until(ExpectedConditions.ElementIsVisible(By.ClassName("sa-icon sa-custom"))).Text;
+        }
+    }
+}
--- Check connection/UnitTest1.cs
+++ Check connection/UnitTest1.cs
@@ -1,14 +1,47 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
 
 namespace Check_connection
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        IWebDriver driver;
+
+        //[OneTimeSetUp] // вызывается перед началом запуска всех тестов
+        //public void OneTimeSetUp()
+        //{
+
+        //}
+
+        //[OneTimeTearDown] //вызывается после завершения всех тестов
+        //public void OneTimeTearDown()
+        //{ 
+
+        //}
+
+        [SetUp] // вызывается перед каждым тестом
+        public void SetUp()
         {
+            driver = new ChromeDriver();
+        }
+
+        [TearDown] // вызывается после каждого теста
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void Connection()
+        {
+            var actual = "Ваш дом подключен";
+
+            Connection avto = new Connection(driver);
+            avto.Action();
+
+            Assert.AreNotEqual(avto.Result, actual);
         }
     }
 }
