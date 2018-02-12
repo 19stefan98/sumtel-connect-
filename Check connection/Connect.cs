4096d49569fbf2b35b54b3b60b1fed5d550c1b08
Степан Митрофанов <stepan.mitrofanov.2015@mail.ru>
2018-02-12 16:49:49 +0400

    d

--- Check connection/Connect.cs
+++ Check connection/Connect.cs
@@ -21,7 +21,8 @@ namespace Check_connection
             driver.Navigate().GoToUrl("http://itech:itech@saint-petersburg.sumtel.itech-test.ru/");
 
             var street = (new WebDriverWait(driver, timeout)).Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id=\"check - connection - block\"]/form/div/div[3]/div/div[1]/label/input")));
-                street.SendKeys("Пловдивская");
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", street);
+            street.SendKeys("Пловдивская");
             var home = driver.FindElement(By.Name("home_corpus"));
                 home.SendKeys("5");
                 home.Submit(); 
